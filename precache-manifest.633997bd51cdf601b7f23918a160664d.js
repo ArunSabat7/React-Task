586362d3ca0e8e647cd96c532fa2a55791ffce86
arun <arunsabat.7@gmail.com>
2020-06-21 13:39:39 +0530

    Updates

---
+++ precache-manifest.633997bd51cdf601b7f23918a160664d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5289cdd3f2f1884abc3fd032feffa8e4",
+    "url": "/React-Task/index.html"
+  },
+  {
+    "revision": "adaf57a1c5d40d95e502",
+    "url": "/React-Task/static/css/main.2e1744ba.chunk.css"
+  },
+  {
+    "revision": "7926273e959ae7751525",
+    "url": "/React-Task/static/js/2.138a21c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Task/static/js/2.138a21c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adaf57a1c5d40d95e502",
+    "url": "/React-Task/static/js/main.26898e6b.chunk.js"
+  },
+  {
+    "revision": "6d860397eb284d7b77fc",
+    "url": "/React-Task/static/js/runtime-main.14e9c4c3.js"
+  }
+]);
